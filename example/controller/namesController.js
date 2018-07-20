23d517699149a05493032b31ac621aaa01e7508a
yvan <57882634@qq.com>
2018-07-20 18:57:08 +0800

    angular1

--- example/controller/namesController.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp', []).controller('namesCtrl', function($scope) {
-  $scope.names = [
-    {name: 'Linus Torvalds', country: 'Finland'},
-    {name: 'Bill Gates', country: 'Sweden'},
-    {name: 'Larry Page', country: 'Denmark'},
-    {name: 'Sergey Brin', country: 'Denmark'},
-    {name: 'Steve Jobs', country: 'Norway'}
-  ];
-});
